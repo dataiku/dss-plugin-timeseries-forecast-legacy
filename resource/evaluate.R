2af5af70a535d3cb2f137b29b08e986e8eea64fe
admin <admin>
2019-12-19 09:02:47 +0000

    Edited file 'resource/evaluate.R'

--- resource/evaluate.R
+++ resource/evaluate.R
@@ -229,6 +229,7 @@ EvaluateModelsCrossval <- function(ts, df, xreg = NULL, modelList, modelParamete
     evalXreg <- NULL
     if (!is.null(xreg)) {
       evalXreg <- as.matrix(xreg[(trainRows + 1):(trainRows + horizon),])
+      colnames(evalXreg) <- colnames(xreg)
     }
     PrintPlugin(paste0("Crossval split ", i ,"/", length(cutoffs), " at cutoff ", cutoffs[i],
       " with ", length(trainTs), " training rows"))
